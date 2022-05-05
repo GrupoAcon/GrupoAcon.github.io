083687aa51869302b53d7aa70624d56f66009229
GrupoAcon <105016680+GrupoAcon@users.noreply.github.com>
2022-05-05 15:34:55 -0500

    initial commit

---
+++ Evaluacion-Fruta/js/detalle.js
@@ -0,0 +1,16 @@
+
+    const dateSetNow = document.querySelector("input[name=Fecha]");
+
+    let date = new Date();
+    let output = String(date.getDate()).padStart(2, '0') + '/' + String(date.getMonth() + 1).padStart(2, '0') + '/' + date.getFullYear();
+    dateSetNow.value=output;
+
+    dateSetNow.addEventListener('click', ()=>{
+    dateSetNow.type='date';
+})
+
+document.querySelector(".naight").addEventListener('click', (e)=>{
+    document.querySelector('body').classList.toggle("dark-mode")
+    document.querySelector(".promediobix").classList.toggle("dark-mode")  
+})
+
